13d996bc4934fca6ce9da3750743f4d3c3f5c115
George.Saeed <gsaeed@yahoo.com>
2020-11-07 01:29:10 +0000

    reverted enemy senses dampening in change # d865ba112

--- Assets/Scripts/Game/EnemySenses.cs
+++ Assets/Scripts/Game/EnemySenses.cs
@@ -230,7 +230,7 @@ namespace DaggerfallWorkshop.Game
 
             byte index = mobile.ClassicSpawnDistanceType;
 
-            classicSpawnXZDist = classicSpawnXZDistArray[index] * MeshReader.GlobalScale /3 * 2;
+            classicSpawnXZDist = classicSpawnXZDistArray[index] * MeshReader.GlobalScale;
             classicSpawnYDistUpper = classicSpawnYDistUpperArray[index] * MeshReader.GlobalScale;
             classicSpawnYDistLower = classicSpawnYDistLowerArray[index] * MeshReader.GlobalScale;
             classicDespawnXZDist = classicDespawnXZDistArray[index] * MeshReader.GlobalScale;
