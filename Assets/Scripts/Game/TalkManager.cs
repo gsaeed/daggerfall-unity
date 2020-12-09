208d3d9dba42172522b47a072681db1ef3900492
George.Saeed <gsaeed@yahoo.com>
2020-12-09 21:23:41 +0000

    Had to change NpcData property settings as set; get; didn't properly link to npcData

--- Assets/Scripts/Game/TalkManager.cs
+++ Assets/Scripts/Game/TalkManager.cs
@@ -192,7 +192,11 @@ namespace DaggerfallWorkshop.Game
             public bool allowGuildResponse = true;
         }
         NPCData npcData;
-        public NPCData NpcData { get; private set; }
+        public NPCData NpcData
+        {
+            get => npcData;
+            private set => npcData = value;
+        }
 
         // type of npc talk partners for a conversion
         enum NPCType
