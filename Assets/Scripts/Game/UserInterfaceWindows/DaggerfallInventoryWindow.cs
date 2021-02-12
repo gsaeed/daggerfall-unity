3b7474f8317a704b1f8861d5e95b2d003edf8b6b
George.Saeed <gsaeed@yahoo.com>
2021-02-12 20:00:56 +0000

    added poisoned weapon logic
    inventory panel now shows any poison on weapons
    Formulas/FormulaHelper was changed to leave poison on weapon after swing if the attacker is the player

--- Assets/Scripts/Game/UserInterfaceWindows/DaggerfallInventoryWindow.cs
+++ Assets/Scripts/Game/UserInterfaceWindows/DaggerfallInventoryWindow.cs
@@ -1136,18 +1136,7 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
             if (item.ItemGroup == ItemGroups.Paintings)
                 tokens = new TextFile.Token[] { new TextFile.Token() { formatting = TextFile.Formatting.Text, text = tokens[tokens.Length - 1].text.Trim() } };
 
-            TextFile.Token[] newTokens = new TextFile.Token[tokens.Length + 2];
-            bool found = false;
-            for (int i = 0,  n = 0; i < tokens.Length; i++)
-            {
-                if (tokens[i].text != null && tokens[i].text.Contains("Worth:"))
-                    found = true;
-                if (i == tokens.Length - 1 && !found)
-                    newTokens[n++] = new TextFile.Token() { formatting = TextFile.Formatting.Text, text = "Worth: " + item.value.ToString() + " gold" };
-                else
-                    newTokens[n++] = tokens[i];
-            }
-            UpdateItemInfoPanel(newTokens);
+            UpdateItemInfoPanel(tokens);
         }
 
         protected void UpdateItemInfoPanel(TextFile.Token[] tokens)
@@ -1602,7 +1591,7 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
             }
         }
 
-        protected void ShowInfoPopup(DaggerfallUnityItem item)
+        protected virtual void ShowInfoPopup(DaggerfallUnityItem item)
         {
             DaggerfallUI.Instance.PlayOneShot(SoundClips.ButtonClick);
             TextFile.Token[] tokens = ItemHelper.GetItemInfo(item, DaggerfallUnity.TextProvider);
@@ -2292,6 +2281,6 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
             OnClose -= OnCloseWindow;
         }
 
-        #endregion  
+        #endregion
     }
 }
