60a114b2bb5cc938fb6b6fe21725fddcf9d9a9a5
George.Saeed <gsaeed@yahoo.com>
2021-03-22 13:46:39 +0000

    added wildcard to console help

--- Assets/Game/Addons/UnityConsole/Console/Scripts/Commands/HelpCommand.cs
+++ Assets/Game/Addons/UnityConsole/Console/Scripts/Commands/HelpCommand.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections.Generic;
 using System.Text;
 using Wenzil.Console;
@@ -21,6 +21,9 @@ namespace Wenzil.Console.Commands
             if (args.Length == 0)
             {
                 return DisplayAvailableCommands();
+            } if (args[0].Contains("*"))
+            {
+                return DisplaySomeCommands(args[0]);
             }
             else
             {
@@ -36,6 +39,47 @@ namespace Wenzil.Console.Commands
             foreach (ConsoleCommand command in ConsoleCommandsDatabase.commands)
             {
                 commandList.Append(string.Format("    <b>{0}</b> - {1}\n", command.name, command.description));
+            }
+
+            commandList.Append("To display details about a specific command, type 'HELP' followed by the command name.");
+            return commandList.ToString();
+        }
+
+        private static string DisplaySomeCommands(string commandString)
+        {
+            commandList.Length = 0; // clear the command list before rebuilding it
+            commandList.Append("<b>Available Commands</b>\n");
+            string commandMatch = "";
+            bool startsWith = false;
+            for (int n = 0; n < commandString.Length; n++)
+            {
+                char a = commandString[n];
+                if (a >= 'a' && a <= 'z')
+                {
+                    commandMatch += a;
+                    if (n == 0)
+                        startsWith = true;
+                } else
+                {
+                    if (startsWith)
+                        break;
+                }
+
+            }
+            
+            foreach (ConsoleCommand command in ConsoleCommandsDatabase.commands)
+            {
+                if (startsWith)
+                {
+                    if (command.name.ToLower().StartsWith(commandMatch))
+                        commandList.Append(string.Format("    <b>{0}</b> - {1}\n", command.name, command.description));
+                }
+                else
+                {
+                if (command.name.ToLower().Contains(commandMatch))
+                    commandList.Append(string.Format("    <b>{0}</b> - {1}\n", command.name, command.description));
+                }
+
             }
 
             commandList.Append("To display details about a specific command, type 'HELP' followed by the command name.");
