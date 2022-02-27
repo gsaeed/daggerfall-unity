c4f3015d5efcf68defb76de15b0cf2f6cc60e311
George.Saeed <gsaeed@yahoo.com>
2022-02-27 15:08:00 +0000

    clear directory when extracting text in the extract text or extract all option in mods

--- Assets/Game/Addons/ModSupport/ModLoaderInterfaceWindow.cs
+++ Assets/Game/Addons/ModSupport/ModLoaderInterfaceWindow.cs
@@ -784,6 +784,23 @@ public class ModLoaderInterfaceWindow : DaggerfallPopupWindow
         MoveNextStage();
     }
 
+    void ClearDirectory(string path)
+    {
+        if (!Directory.Exists(path))
+            return;
+
+        System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+        foreach (FileInfo file in di.GetFiles())
+        {
+            file.Delete();
+        }
+        foreach (DirectoryInfo dir in di.GetDirectories())
+        {
+            dir.Delete(true);
+        }
+    }
+
     void ExtractFilesButton_OnMouseClick(BaseScreenComponent sender, Vector2 position)
     {
         if (modSettings.Length < 1)
@@ -801,6 +818,9 @@ public class ModLoaderInterfaceWindow : DaggerfallPopupWindow
             return;
 
         string path = Path.Combine(DaggerfallUnityApplication.PersistentDataPath, "Mods", "ExtractedFiles", mod.FileName);
+
+        ClearDirectory(path);
+
         Directory.CreateDirectory(path);
 
         for (int i = 0; i < assets.Length; i++)
@@ -899,6 +919,7 @@ public class ModLoaderInterfaceWindow : DaggerfallPopupWindow
                 continue;
 
             string path = Path.Combine(Application.persistentDataPath, "Mods", "ExtractedFiles", mod.FileName);
+            ClearDirectory(path);
             Directory.CreateDirectory(path);
 
             for (int i = 0; i < assets.Length; i++)
