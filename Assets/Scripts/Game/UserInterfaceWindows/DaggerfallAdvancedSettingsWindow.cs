1ee0aba9d4d879c19462da1b1cde5aedbe8c5e32
George <gsaeed@yahoo.com>
2022-10-23 14:34:58 -0700

    new settings.ini setting MuteMusic
    If set to true then music is set to zero and can't be changed in game.

--- Assets/Scripts/Game/UserInterfaceWindows/DaggerfallAdvancedSettingsWindow.cs
+++ Assets/Scripts/Game/UserInterfaceWindows/DaggerfallAdvancedSettingsWindow.cs
@@ -267,9 +267,17 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
             soundVolume.DisplayUnits = 100;
             soundVolume.OnScroll += SoundVolume_OnScroll;
             soundVolume.OnMouseUp += SoundVolume_OnMouseUp;
-            musicVolume = AddSlider(rightPanel, "musicVolume", 0, 100, DaggerfallUnity.Settings.MusicVolume * 100);
-            musicVolume.DisplayUnits = 100;
-            musicVolume.OnScroll += MusicVolume_OnScroll;
+            if (DaggerfallUnity.Settings.MuteMusic)
+            {
+                musicVolume = AddSlider(rightPanel, "muteMusicVolume", 0, 0, DaggerfallUnity.Settings.MusicVolume * 100);
+                musicVolume.OnScroll -= MusicVolume_OnScroll;
+            }
+            else
+            {
+                musicVolume = AddSlider(rightPanel, "musicVolume", 0, 100, DaggerfallUnity.Settings.MusicVolume * 100);
+                musicVolume.DisplayUnits = 100;
+                musicVolume.OnScroll += MusicVolume_OnScroll;
+            }
 
             // Spells
             AddSectionTitle(rightPanel, "spells");
@@ -439,7 +447,10 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
 
             DaggerfallUnity.Settings.AlternateMusic = alternateMusic.IsChecked;
             DaggerfallUnity.Settings.SoundVolume = soundVolume.GetValue() / 100f;
-            DaggerfallUnity.Settings.MusicVolume = musicVolume.GetValue() / 100f;
+            if (DaggerfallUnity.Settings.MuteMusic)
+                DaggerfallUnity.Settings.MusicVolume = 0;
+            else
+                DaggerfallUnity.Settings.MusicVolume = musicVolume.GetValue() / 100f;
 
             DaggerfallUnity.Settings.EnableSpellLighting = spellLighting.IsChecked;
             DaggerfallUnity.Settings.EnableSpellShadows = spellShadows.IsChecked;
@@ -841,6 +852,8 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
 
          private void MusicVolume_OnScroll()
         {
+            if (DaggerfallUnity.Settings.MuteMusic)
+                musicVolume.SetValue(0);
             DaggerfallUnity.Settings.MusicVolume = musicVolume.GetValue() / 100f;
         }
 
