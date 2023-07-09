facf2ba05b37ccabd3fa36428116c77be5f12f10
George.Saeed <gsaeed@yahoo.com>
2023-07-09 16:16:27 +0100

    moved min max rolls to parms and moved pawn shops sell horses to parm.  add and enable collider for custom activations

--- Assets/Scripts/Game/PlayerActivate.cs
+++ Assets/Scripts/Game/PlayerActivate.cs
@@ -164,17 +164,18 @@ namespace DaggerfallWorkshop.Game
         private static void HandleRegisterCustomActivation(Mod provider, string goFlatModelName, CustomActivation customActivation, float activationDistance)
         {
             string withoutRep, withRep;
+            bool sentWithRep = false;
             if (goFlatModelName.EndsWith(" [Replacement]"))
             {
                 withRep = goFlatModelName;
                 withoutRep = goFlatModelName.Substring(0, goFlatModelName.Length - " [Replacement]".Length);
+                sentWithRep = true;
             }
             else
             {
                 withRep = goFlatModelName + " [Replacement]";
                 withoutRep = goFlatModelName;
             }
-
             DaggerfallUnity.LogMessage($"HandleRegisterCustomActivation: {withRep} from mod: {provider.FileName}", true);
             CustomModActivation existingActivation;
             if (customModActivations.TryGetValue(withRep, out existingActivation))
@@ -195,6 +196,9 @@ namespace DaggerfallWorkshop.Game
             else
             {
                 customModActivations[withRep] = new CustomModActivation(customActivation, activationDistance, provider);
+                Debug.Log(sentWithRep
+                    ? $"Custom Activation: custom activation registered for {withRep} from {provider.Title}"
+                    : $"Custom Activation: custom activation registered for {withRep} auto added in support of {provider.Title}");
             }
 
             DaggerfallUnity.LogMessage($"HandleRegisterCustomActivation: {withoutRep} from mod: {provider.FileName}", true);
@@ -217,6 +221,9 @@ namespace DaggerfallWorkshop.Game
             else
             {
                 customModActivations[withoutRep] = new CustomModActivation(customActivation, activationDistance, provider);
+                Debug.Log(sentWithRep
+                    ? $"Custom Activation: custom activation registered for {withoutRep} auto added in support of {provider.Title}"
+                    : $"Custom Activation: custom activation registered for {withoutRep} from {provider.Title}");
             }
         }
 
