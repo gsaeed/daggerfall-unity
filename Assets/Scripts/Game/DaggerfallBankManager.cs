2192145262b15456aa3c3e782336a9a09292daa2
George.Saeed <gsaeed@yahoo.com>
2023-07-17 14:11:04 +0100

    Changed housePriceMult from private const to Public static so it can be overriden.
    Added override in LimitedGoldShops since that mod lowers gold collection so getting to 500K to buy a house is too difficult

--- Assets/Scripts/Game/DaggerfallBankManager.cs
+++ Assets/Scripts/Game/DaggerfallBankManager.cs
@@ -91,7 +91,7 @@ namespace DaggerfallWorkshop.Game.Banking
 
         [Obsolete("Replace with 'goldPieceWeightInKg'")] public const float goldUnitWeightInKg = 0.0025f;
         private const float deedSellMult = 0.85f;
-        private const float housePriceMult = 1280f;
+        public static float housePriceMult = 1280f;
         private const uint loanRepayMinutes = DaggerfallDateTime.DaysPerYear * DaggerfallDateTime.MinutesPerDay;
 
         #region Ships:
