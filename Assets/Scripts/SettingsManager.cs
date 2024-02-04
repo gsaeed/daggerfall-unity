31bcc30f18c879392a03e29c032ff55d4155bb98
George.Saeed <gsaeed@yahoo.com>
2024-02-04 17:06:26 +0000

    added multiplier for door bashing

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -218,6 +218,7 @@ namespace DaggerfallWorkshop
         public float ColorBoostRadius { get; set; }
         public float ColorBoostIntensity { get; set; }
         public bool LogHudMessages { get; set; }
+        public int DoorBashEaseMultiplier { get; set; }
         public float ColorBoostDungeonScale { get; set; }
         public float ColorBoostExteriorScale { get; set; }
         public float ColorBoostInteriorScale { get; set; }
@@ -522,6 +523,8 @@ namespace DaggerfallWorkshop
             EnableVitalsIndicators = GetBool(sectionGUI, "EnableVitalsIndicators");
             DisplayHUDScaleAdjust = GetFloat(sectionGUI, "DisplayHUDScaleAdjust");
             LogHudMessages = GetBool(sectionGUI, "LogHudMessages");
+            DoorBashEaseMultiplier = GetInt(sectionGUI, "DoorBashEaseMultiplier");
+
             DisplayMobileEnemyHealthStatus = GetBool(sectionGUI, "DisplayMobileEnemyHealthStatus");
 
             SDFFontRendering = GetBool(sectionGUI, "SDFFontRendering");
@@ -754,6 +757,7 @@ namespace DaggerfallWorkshop
             SetBool(sectionGUI, "HideLoginName", HideLoginName);
             SetFloat(sectionGUI, "DisplayHUDScaleAdjust", DisplayHUDScaleAdjust);
             SetBool(sectionGUI, "LogHudMessages", LogHudMessages);
+            SetInt(sectionGUI, "DoorBashEaseMultiplier", DoorBashEaseMultiplier);
             SetBool(sectionGUI, "DisplayMobileEnemyHealthStatus", DisplayMobileEnemyHealthStatus);
 
             SetInt(sectionMeleeAttacks, "MeleeAttackDetection", MeleeAttackDetection);
