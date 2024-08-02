d15334a2cbaff7f1f6971b98a694dfc69cbc0559
George.Saeed <gsaeed@yahoo.com>
2024-08-02 14:17:53 +0100

    Send dye color to TextureReplacement in order to get the right png.  Before the change, it was getting the png without the dye color Archive_Record-0.png instead of Archive_Record-0-DyeColor.png

--- Assets/Scripts/Game/Items/ItemHelper.cs
+++ Assets/Scripts/Game/Items/ItemHelper.cs
@@ -420,6 +420,7 @@ namespace DaggerfallWorkshop.Game.Items
                     record += 1;
             }
 
+            bool isWeaponArmor = (item.ItemGroup == ItemGroups.Armor || item.ItemGroup == ItemGroups.Weapons);
             // Use world texture archive if inventory texture not set
             // Examples are gold pieces and wayrest painting
             if (archive == 0 && record == 0)
@@ -437,7 +438,7 @@ namespace DaggerfallWorkshop.Game.Items
 
             // Load image data
             string filename = TextureFile.IndexToFileName(archive);
-            ImageData data = ImageReader.GetImageData(filename, record, 0, true, false, allowAnimation);
+            ImageData data = ImageReader.GetImageData(filename, item.dyeColor, record, 0, true, false, allowAnimation, 0, isWeaponArmor);
             if (data.type == ImageTypes.None)
                 throw new Exception("GetItemImage() could not load image data.");
 
