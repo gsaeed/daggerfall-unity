5caa430d0c7a93e1f2d40c56300fd3072df1b608
George.Saeed <gsaeed@yahoo.com>
2024-09-05 12:48:18 +0100

    added asset count to build mod file and used inline replace versus custom function RemoveComma

--- Assets/Game/Addons/ModSupport/ModLoaderInterfaceWindow.cs
+++ Assets/Game/Addons/ModSupport/ModLoaderInterfaceWindow.cs
@@ -1198,15 +1198,30 @@ public class ModLoaderInterfaceWindow : DaggerfallPopupWindow
     {
         ModManager.Instance.SortMods();
         string path = Path.Combine(Application.persistentDataPath, "Mods");
+        Dictionary<string, int> assetCount = new Dictionary<string, int>();
+        foreach (Mod m in ModManager.Instance.Mods)
+        {
+           if (m.AssetNames != null && m.AssetNames.Length > 0)
+                foreach (string a in m.AssetNames)
+                {
+                    if (!assetCount.ContainsKey(a))
+                        assetCount.Add(a, 1);
+                    else
+                        assetCount[a]++;
+                }
+        }
 
         File.WriteAllText(path + @"\mods.csv", "Mod Title,Mod Filename,Enabled,Load Priority, Asset List, Count" + Environment.NewLine);
         foreach (Mod m in ModManager.Instance.Mods)
         {
-            File.AppendAllText(path + @"\mods.csv", $"{RemoveComma(m.Title)}, {RemoveComma(m.FileName)}, {m.Enabled}, {m.LoadPriority}" + Environment.NewLine);
-            if (m.AssetNames != null)
+            File.AppendAllText(path + @"\mods.csv", $"{m.Title.Trim().Replace(',','-')},{m.FileName.Trim().Replace(',','-')},{m.Enabled},{m.LoadPriority}" + Environment.NewLine);
+            if (m.AssetNames != null && m.AssetNames.Length > 0)
                 foreach (string a in m.AssetNames)
                 {
-                    File.AppendAllText(path + @"\mods.csv", $"{RemoveComma(m.Title)}, {RemoveComma(m.FileName)}, {m.Enabled},{m.LoadPriority}, {a}, 1" + Environment.NewLine);
+                    int cnt;
+                    if (!assetCount.TryGetValue(a, out cnt))
+                        cnt = 1;
+                    File.AppendAllText(path + @"\mods.csv", $"{m.Title.Trim().Replace(',','-')},{m.FileName.Trim().Replace(',','-')},{m.Enabled},{m.LoadPriority},{a.Trim().Replace(',','-')},{cnt}" + Environment.NewLine);
                 }
         }
 
@@ -1330,7 +1345,6 @@ public class ModLoaderInterfaceWindow : DaggerfallPopupWindow
         string[] strArray = str.Split(',');
         return String.Join(" ", strArray);
     }
-
 
     void ShowModDescriptionPopUp_OnMouseClick(BaseScreenComponent sender, Vector2 position)
     {
