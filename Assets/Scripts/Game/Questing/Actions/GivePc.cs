5c02d9334e83159b3a2cd35347be8a97f6c55d1d
George.Saeed <gsaeed@yahoo.com>
2024-11-22 16:21:52 +0000

    improved error messaging on event fail

--- Assets/Scripts/Game/Questing/Actions/GivePc.cs
+++ Assets/Scripts/Game/Questing/Actions/GivePc.cs
@@ -246,9 +246,18 @@ namespace DaggerfallWorkshop.Game.Questing.Actions
                 }
                 catch (Exception e)
                 {
+                    var del = OnOfferPending;
+                    var str = string.Empty;
                     var currMethod = new StackTrace().GetFrame(0).GetMethod();
-                    Debug.LogError($"Exception running {currMethod.ReflectedType}:{currMethod} - {e.Message}");
-                    Debug.LogError($"{e.ToString()}");
+                    var className = currMethod.ReflectedType != null ? currMethod.ReflectedType.FullName : string.Empty;
+                    if (del != null && del.Method != null && del.Method.DeclaringType != null)
+                    {
+                        className = del.Method.DeclaringType.FullName;
+                        currMethod = del.Method;
+                    }
+
+                    str += $"Exception running {className}.{currMethod.Name}\n{e.Message}\n{e}";
+                    UnityEngine.Debug.LogError(str);
                 }
         }
 
